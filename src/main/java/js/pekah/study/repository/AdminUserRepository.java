367da8642cc8c35a28677e2ae50ba5a5e0e3c998
JosephNaa <js.pekah@gmail.com>
2021-02-18 21:05:54 +0900

    add Repository

---
+++ src/main/java/js/pekah/study/repository/AdminUserRepository.java
@@ -0,0 +1,9 @@
+package js.pekah.study.repository;
+
+import js.pekah.study.model.entity.AdminUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AdminUserRepository extends JpaRepository<AdminUser, Long> {
+}
---
+++ src/main/java/js/pekah/study/repository/OrderGroupRepository.java
@@ -0,0 +1,9 @@
+package js.pekah.study.repository;
+
+import js.pekah.study.model.entity.OrderGroup;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderGroupRepository extends JpaRepository<OrderGroup, Long> {
+}
