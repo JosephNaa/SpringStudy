c18c1e2adfa812fcb23fe03e6ca76527a203df1c
JosephNaa <js.pekah@gmail.com>
2021-02-20 19:34:59 +0900

    add service

---
+++ src/main/java/js/pekah/study/service/UserApiLogicService.java
@@ -0,0 +1,108 @@
+package js.pekah.study.service;
+
+import js.pekah.study.ifs.CrudInterface;
+import js.pekah.study.model.entity.User;
+import js.pekah.study.model.network.Header;
+import js.pekah.study.model.network.request.UserApiRequest;
+import js.pekah.study.model.network.response.UserApiResponse;
+import js.pekah.study.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Service
+public class UserApiLogicService implements CrudInterface<UserApiRequest, UserApiResponse> {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public Header<UserApiResponse> create(Header<UserApiRequest> request) {
+
+        // 1. request data
+        UserApiRequest userApiRequest = request.getData();
+
+        // 2. user 생성
+        User user = User.builder()
+                .account(userApiRequest.getAccount())
+                .password(userApiRequest.getPassword())
+                .status("REGISTERED")
+                .phoneNumber(userApiRequest.getPhoneNumber())
+                .email(userApiRequest.getEmail())
+                .registeredAt(LocalDateTime.now())
+                .build();
+
+        User newUser = userRepository.save(user);
+
+        // 3. 생성된 데이터 -> UserApiResponse return
+        return response(newUser);
+    }
+
+    @Override
+    public Header<UserApiResponse> read(Long id) {
+
+        return userRepository
+                .findById(id)
+                .map(user -> response(user))
+                .orElseGet(() -> Header.ERROR("no user data"));
+    }
+
+    @Override
+    public Header<UserApiResponse> update(Header<UserApiRequest> request) {
+
+        UserApiRequest userApiRequest = request.getData();
+
+        Optional<User> optional = userRepository.findById(userApiRequest.getId());
+
+        return optional.map(user -> {
+            user.setAccount(userApiRequest.getAccount())
+                    .setPassword(userApiRequest.getPassword())
+                    .setStatus(userApiRequest.getStatus())
+                    .setPhoneNumber(userApiRequest.getPhoneNumber())
+                    .setEmail(userApiRequest.getEmail())
+                    .setRegisteredAt(userApiRequest.getRegisteredAt())
+                    .setUnregisteredAt(userApiRequest.getUnregisteredAt())
+                    ;
+
+            return user;
+        })
+        .map(user -> userRepository.save(user))
+        .map(updateUser -> response(updateUser))
+        .orElseGet(() -> Header.ERROR("no update data"));
+    }
+
+    @Override
+    public Header delete(Long id) {
+
+        Optional<User> optional = userRepository.findById(id);
+
+        return optional.map(user -> {
+            userRepository.delete(user);
+
+            return Header.OK();
+        })
+        .orElseGet(() -> Header.ERROR("no delete data"));
+    }
+
+    private Header<UserApiResponse> response(User user) {
+        //user -> userApiResponse
+
+        UserApiResponse userApiResponse = UserApiResponse.builder()
+                .id(user.getId())
+                .account(user.getAccount())
+                .password(user.getPassword())
+                .email(user.getEmail())
+                .phoneNumber(user.getPhoneNumber())
+                .status(user.getStatus())
+                .registeredAt(user.getRegisteredAt())
+                .unregisteredAt(user.getUnregisteredAt())
+                .build();
+
+        // Header + data return
+        return Header.OK(userApiResponse);
+
+    }
+
+}
