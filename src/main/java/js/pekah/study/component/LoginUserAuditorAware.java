7c2711d97188b4075ede824e353b6240eba4f5c9
JosephNaa <js.pekah@gmail.com>
2021-02-20 16:32:34 +0900

    add component

---
+++ src/main/java/js/pekah/study/component/LoginUserAuditorAware.java
@@ -0,0 +1,14 @@
+package js.pekah.study.component;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class LoginUserAuditorAware implements AuditorAware<String> {
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.of("AdminServer");
+    }
+}
