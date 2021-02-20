791bc3a7820664da0555b8c45b8bffd23871e662
JosephNaa <js.pekah@gmail.com>
2021-02-20 19:32:25 +0900

    add Crud interface

---
+++ src/main/java/js/pekah/study/ifs/CrudInterface.java
@@ -0,0 +1,14 @@
+package js.pekah.study.ifs;
+
+import js.pekah.study.model.network.Header;
+
+public interface CrudInterface<Req, Res> {
+
+    Header<Res> create(Header<Req> request);
+
+    Header<Res> read(Long id);
+
+    Header<Res> update(Header<Req> request);
+
+    Header delete(Long id);
+}
