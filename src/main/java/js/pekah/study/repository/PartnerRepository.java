7ecaf937d5950fa41f4f4679612a7eae90b79383
JosephNaa <js.pekah@gmail.com>
2021-02-21 19:17:41 +0900

    add findBy

--- src/main/java/js/pekah/study/repository/PartnerRepository.java
+++ src/main/java/js/pekah/study/repository/PartnerRepository.java
@@ -1,9 +1,14 @@
-package js.pekah.backend.repository;
+package js.pekah.study.repository;
 
-import js.pekah.backend.model.entity.Partner;
+import js.pekah.study.model.entity.Category;
+import js.pekah.study.model.entity.Partner;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PartnerRepository extends JpaRepository<Partner, Long> {
+
+    List<Partner> findByCategory(Category category);
 }
