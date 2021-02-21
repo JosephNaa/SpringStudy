9b777c2251a19344e4f71f0c211b5ba9c97ed2d9
JosephNaa <js.pekah@gmail.com>
2021-02-21 17:30:18 +0900

    add response

---
+++ src/main/java/js/pekah/study/model/network/response/CategoryApiResponse.java
@@ -0,0 +1,20 @@
+package js.pekah.study.model.network.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CategoryApiResponse {
+
+    private Long id;
+
+    private String type;
+
+    private String title;
+
+}
