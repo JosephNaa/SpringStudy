45a5bd3f63789ed82b1a955158255048caba40b0
JosephNaa <js.pekah@gmail.com>
2021-02-21 20:43:17 +0900

    front page

---
+++ src/main/java/js/pekah/study/service/AdminMenuService.java
@@ -0,0 +1,20 @@
+package js.pekah.study.service;
+
+import js.pekah.study.model.front.AdminMenu;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class AdminMenuService {
+
+    public List<AdminMenu> getAdminMenu(){
+
+        return Arrays.asList(
+                AdminMenu.builder().title("고객 관리").url("/pages/user").code("user").build()
+        );
+
+    }
+
+}
